d91f171207de6c5981d089ee8b5357b0cb9123b4
yyzhou-gh <85494067+yyzhou-gh@users.noreply.github.com>
2022-11-24 20:50:25 +0800

    Update code.R

--- 5wSNP/code.R
+++ 5wSNP/code.R
@@ -3,12 +3,12 @@ setwd("/home/yyzhou/CUBIC/5wSNP")
 #Load all omics data   
     load("/home/yyzhou/CUBIC/5wSNP/dataset/trainingset/snp.394.Rdata")  #50000 SNP
     load("/home/yyzhou/CUBIC/5wSNP/dataset/trainingset/Transcriptomics.394.Rdata")  #26743 Transcripts 
-    load("/home/yyzhou/CUBIC/5wSNP/dataset/trainingset/Phe.19.394.Rdata") #19 phenotypes
+    load("/home/yyzhou/CUBIC/5wSNP/dataset/trainingset/Sub_Trait.394.Rdata") #19 Sub_Traits
     load("/home/yyzhou/CUBIC/5wSNP/dataset/trainingset/EW.394.Rdata")
     load("/home/yyzhou/CUBIC/5wSNP/dataset/trainingset/cv.group.Rdata")
 
     #creat dataframe
-    CUBIC.dataframe<-data.frame(Phe.19.394,EW.394,snp.394,Transcriptomics.394)
+    CUBIC.dataframe<-data.frame(Sub_Trait.394,EW.394,snp.394,Transcriptomics.394)
 
 #create list  
     trainingset<-list()
@@ -16,12 +16,12 @@ setwd("/home/yyzhou/CUBIC/5wSNP")
     
     CUBIC.G.training<-list()
     CUBIC.E.training<-list()
-    CUBIC.19Phe.training<-list()
+    CUBIC.Sub_Trait.training<-list()
     CUBIC.EW.training<-list()
 
     CUBIC.G.valid<-list()
     CUBIC.E.valid<-list()
-    CUBIC.19Phe.valid<-list()   
+    CUBIC.Sub_Trait.valid<-list()   
     CUBIC.EW.valid<-list()
 
 for(i in 1:10){
@@ -29,14 +29,14 @@ for(i in 1:10){
       trainingset[[i]]<-CUBIC.dataframe[setdiff(rownames(snp.394),cv.group[[i]]),]
       CUBIC.G.training[[i]]<-as.matrix(trainingset[[i]][,21:50020])
       CUBIC.E.training[[i]]<-as.matrix(trainingset[[i]][,50021:76763])
-      CUBIC.19Phe.training[[i]]<-as.matrix(trainingset[[i]][,1:19])	
+      CUBIC.Sub_Trait.training[[i]]<-as.matrix(trainingset[[i]][,1:19])	
       CUBIC.EW.training[[i]]<-as.matrix(trainingset[[i]][,20])	
       colnames(CUBIC.EW.training[[i]])<-'EW'
   #create validset  
       validset[[i]]<-CUBIC.dataframe[cv.group[[i]],]  	
       CUBIC.G.valid[[i]]<-as.matrix(validset[[i]][,21:50020])
       CUBIC.E.valid[[i]]<-as.matrix(validset[[i]][,50021:76763])
-      CUBIC.19Phe.valid[[i]]<-as.matrix(validset[[i]][,1:19])
+      CUBIC.Sub_Trait.valid[[i]]<-as.matrix(validset[[i]][,1:19])
       CUBIC.EW.valid[[i]]<-as.matrix(validset[[i]][,20]) 
       colnames(CUBIC.EW.valid[[i]])<-'EW'		
       ifelse(i==1,CUBIC.E.valid.10merge<-CUBIC.E.valid[[i]],CUBIC.E.valid.10merge<-rbind(CUBIC.E.valid.10merge,CUBIC.E.valid[[i]]))		  	  
@@ -82,7 +82,7 @@ evaluation<-function(Pred,True){
   return(eva.matrix)
 }
 
-##construct G2GPG2Phe2EW model
+##construct G2GPG2Sub_Trait2EW model
 #construct the first layer
 args=commandArgs(T)
 i=as.numeric(args[1])
@@ -100,10 +100,10 @@ for(i in 1:10){
 Predictability.E.1L<-evaluation(Pred.E.valid.1L.10merge,CUBIC.E.valid.10merge)
 #construct the second and third layer
 Predictability.EW.3L<-matrix(,18,2) 
-Pred.19Phe.2L<-list()
+Pred.Sub_Trait.2L<-list()
 Pred.EW.3L<-list()
 for(j in 1:18){
-  Pred.19Phe.2L[[j]]<-list();length(Pred.19Phe.2L[[j]])<-10
+  Pred.Sub_Trait.2L[[j]]<-list();length(Pred.Sub_Trait.2L[[j]])<-10
   Pred.EW.3L[[j]]<-list();length(Pred.EW.3L[[j]])<-10
   alpha<-(j-1)*0.05   
   ids.Pred.GPGs.1L<-rownames(Predictability.E.1L)[which(Predictability.E.1L[,1]>alpha)]
@@ -113,9 +113,9 @@ for(j in 1:18){
   for(i in 1:10){	
       Pred.GPGs.1L.training[[i]]<-Pred.E.training.1L[[i]][,ids.Pred.GPGs.1L]
       Pred.GPGs.1L.valid[[i]]<-Pred.E.valid.1L[[i]][,ids.Pred.GPGs.1L]
-      Pred.19Phe.2L[[j]][[i]]<-training.LASSO(Pred.GPGs.1L.training[[i]],Pred.GPGs.1L.valid[[i]],
-                                  CUBIC.19Phe.training[[i]],CUBIC.19Phe.valid[[i]])  
-      Pred.EW.3L[[j]][[i]]<-training.LASSO(Pred.19Phe.2L[[j]][[i]][[1]],Pred.19Phe.2L[[j]][[i]][[2]],
+      Pred.Sub_Trait.2L[[j]][[i]]<-training.LASSO(Pred.GPGs.1L.training[[i]],Pred.GPGs.1L.valid[[i]],
+                                  CUBIC.Sub_Trait.training[[i]],CUBIC.Sub_Trait.valid[[i]])  
+      Pred.EW.3L[[j]][[i]]<-training.LASSO(Pred.Sub_Trait.2L[[j]][[i]][[1]],Pred.Sub_Trait.2L[[j]][[i]][[2]],
                                       CUBIC.EW.training[[i]],CUBIC.EW.valid[[i]])     
       ifelse(i==1,Pred.EW.3L.valid.10merge<-Pred.EW.3L[[j]][[i]][[2]],Pred.EW.3L.valid.10merge<-rbind(Pred.EW.3L.valid.10merge,Pred.EW.3L[[j]][[i]][[2]]))
   }    
@@ -131,12 +131,12 @@ for(j in 1:18){
 ##construct other models
 Pred.G2EW<-list()
 Pred.all_E2EW<-list()
-Pred.19Phe2EW<-list()
+Pred.Sub_Trait2EW<-list()
 Pred.GPGs.training<-list()
 Pred.GPGs.valid<-list()
-Pred.GPGs_19Phe.training<-list()
-Pred.GPGs_19Phe.valid<-list()
-Pred.GPGs_19Phe2EW<-list()
+Pred.GPG&Sub_Trait.training<-list()
+Pred.GPG&Sub_Trait.valid<-list()
+Pred.GPG&Sub_Trait2EW<-list()
 #G to E to EW
 Predictability.G2GPG2EW.394<-matrix(,18,2)
 Pred.G2GPG2EW<-list()
@@ -168,30 +168,30 @@ for(i in 1:10){
   #all Transcriptomics to EW
     Pred.all_E2EW[[i]]<-training.LASSO(CUBIC.E.training[[i]],CUBIC.E.valid[[i]],CUBIC.EW.training[[i]],CUBIC.EW.valid[[i]]) 
     ifelse(i==1,Pred.all_E2EW.10merge<-Pred.all_E2EW[[i]][[2]],Pred.all_E2EW.10merge<-rbind(Pred.all_E2EW.10merge,Pred.all_E2EW[[i]][[2]]))		  
-  #19Phe to EW
-    Pred.19Phe2EW[[i]]<-training.LASSO(CUBIC.19Phe.training[[i]],CUBIC.19Phe.valid[[i]],CUBIC.EW.training[[i]],CUBIC.EW.valid[[i]]) 
-    ifelse(i==1,Pred.19Phe2EW.10merge<-Pred.19Phe2EW[[i]][[2]],Pred.19Phe2EW.10merge<-rbind(Pred.19Phe2EW.10merge,Pred.19Phe2EW[[i]][[2]]))		  	  
-  #G to GPG&Phe to EW
+  #Sub_Trait to EW
+    Pred.Sub_Trait2EW[[i]]<-training.LASSO(CUBIC.Sub_Trait.training[[i]],CUBIC.Sub_Trait.valid[[i]],CUBIC.EW.training[[i]],CUBIC.EW.valid[[i]]) 
+    ifelse(i==1,Pred.Sub_Trait2EW.10merge<-Pred.Sub_Trait2EW[[i]][[2]],Pred.Sub_Trait2EW.10merge<-rbind(Pred.Sub_Trait2EW.10merge,Pred.Sub_Trait2EW[[i]][[2]]))		  	  
+  #G to GPG&Sub-Trait to EW
     Pred.GPGs.training[[i]]<-Pred.E.training.1L[[i]][,ids.Pred.GPGs]
     Pred.GPGs.valid[[i]]<-Pred.E.valid.1L[[i]][,ids.Pred.GPGs]
-    Pred.GPGs_19Phe.training[[i]]<-cbind(Pred.GPGs.training[[i]],Pred.19Phe.2L[[best.alpha2]][[i]][[1]])
-    Pred.GPGs_19Phe.valid[[i]]<-cbind(Pred.GPGs.valid[[i]],Pred.19Phe.2L[[best.alpha2]][[i]][[2]])
-    Pred.GPGs_19Phe2EW[[i]]<-training.LASSO(Pred.GPGs_19Phe.training[[i]],Pred.GPGs_19Phe.valid[[i]],CUBIC.EW.training[[i]],CUBIC.EW.valid[[i]]) 
-    ifelse(i==1,Pred.GPGs_19Phe2EW.10merge<-Pred.GPGs_19Phe2EW[[i]][[2]],Pred.GPGs_19Phe2EW.10merge<-rbind(Pred.GPGs_19Phe2EW.10merge,Pred.GPGs_19Phe2EW[[i]][[2]]))
+    Pred.GPG&Sub_Trait.training[[i]]<-cbind(Pred.GPGs.training[[i]],Pred.Sub_Trait.2L[[best.alpha2]][[i]][[1]])
+    Pred.GPG&Sub_Trait.valid[[i]]<-cbind(Pred.GPGs.valid[[i]],Pred.Sub_Trait.2L[[best.alpha2]][[i]][[2]])
+    Pred.GPG&Sub_Trait2EW[[i]]<-training.LASSO(Pred.GPG&Sub_Trait.training[[i]],Pred.GPG&Sub_Trait.valid[[i]],CUBIC.EW.training[[i]],CUBIC.EW.valid[[i]]) 
+    ifelse(i==1,Pred.GPG&Sub_Trait2EW.10merge<-Pred.GPG&Sub_Trait2EW[[i]][[2]],Pred.GPG&Sub_Trait2EW.10merge<-rbind(Pred.GPG&Sub_Trait2EW.10merge,Pred.GPG&Sub_Trait2EW[[i]][[2]]))
 }
 
 Predictability.G2EW.394<-evaluation(Pred.G2EW.10merge,CUBIC.EW.valid.10merge)
 Predictability.all_E2EW.394<-evaluation(Pred.all_E2EW.10merge,CUBIC.EW.valid.10merge)
-Predictability.19Phe2EW.394<-evaluation(Pred.19Phe2EW.10merge,CUBIC.EW.valid.10merge)
-Predictability.GPGs_19Phe2EW.394<-evaluation(Pred.GPGs_19Phe2EW.10merge,CUBIC.EW.valid.10merge)
+Predictability.Sub_Trait2EW.394<-evaluation(Pred.Sub_Trait2EW.10merge,CUBIC.EW.valid.10merge)
+Predictability.GPG&Sub_Trait2EW.394<-evaluation(Pred.GPG&Sub_Trait2EW.10merge,CUBIC.EW.valid.10merge)
 save(Predictability.G2EW.394,file="/home/yyzhou/CUBIC/5wSNP/result/Predictability.training/Predictability.G2EW.394.Rdata")
 save(Predictability.all_E2EW.394,file="/home/yyzhou/CUBIC/5wSNP/result/Predictability.training/Predictability.all_E2EW.394.Rdata")
-save(Predictability.19Phe2EW.394,file="/home/yyzhou/CUBIC/5wSNP/result/Predictability.training/Predictability.19Phe2EW.394.Rdata")
-save(Predictability.GPGs_19Phe2EW.394,file="/home/yyzhou/CUBIC/5wSNP/result/Predictability.training/Predictability.GPGs_19Phe2EW.394.Rdata")
+save(Predictability.Sub_Trait2EW.394,file="/home/yyzhou/CUBIC/5wSNP/result/Predictability.training/Predictability.Sub_Trait2EW.394.Rdata")
+save(Predictability.GPG&Sub_Trait2EW.394,file="/home/yyzhou/CUBIC/5wSNP/result/Predictability.training/Predictability.GPG&Sub_Trait2EW.394.Rdata")
 
 ##construct model in trainingset and test in independent testingset
 load('/home/yyzhou/CUBIC20211104/datasets/test.snp.Rdata')
-load('/home/yyzhou/CUBIC20211104/datasets/test.19Phe.Rdata') 
+load('/home/yyzhou/CUBIC20211104/datasets/test.Sub_Trait.Rdata') 
 load('/home/yyzhou/CUBIC20211104/datasets/test.EW.Rdata') 
 testing.LASSO<-function(X.training,X.testing,Y.training,model.name){ 
     p<-ncol(Y.training)
@@ -217,17 +217,17 @@ testing.LASSO<-function(X.training,X.testing,Y.training,model.name){
     return(list(PredY.training,PredY.testing))
 }
 
-#1.G to GPGs to 19Phe to EW
+#1.G to GPGs to Sub_Trait to EW
 #1.1.The first layer for G to GPG
 Pred.GPGs.1L<-testing.LASSO(snp.394,test.snp,Transcriptomics.GPG,'G2GPG.1L')
 Pred.GPGs.1L.394<-Pred.GPGs.1L[[1]]
 Pred.GPGs.1L.testing<-Pred.GPGs.1L[[2]]
-#1.2.The second layer for predicted GPG to 19Phe 
-Pred.19Phe.2L<-testing.LASSO(Pred.GPGs.1L.394,Pred.GPGs.1L.testing,Phe.19.394,'GPG2Phe.2L')
-Pred.19Phe.2L.394<-Pred.19Phe.2L[[1]]
-Pred.19Phe.2L.testing<-Pred.19Phe.2L[[2]]
-#1.3.The third layer for predicted 19Phe to EW 
-Pred.EW.3L<-testing.LASSO(Pred.19Phe.2L.394,Pred.19Phe.2L.testing,EW.394,'Phe2EW.3L')
+#1.2.The second layer for predicted GPG to Sub_Trait 
+Pred.Sub_Trait.2L<-testing.LASSO(Pred.GPGs.1L.394,Pred.GPGs.1L.testing,Sub_Trait.394,'GPG2Phe.2L')
+Pred.Sub_Trait.2L.394<-Pred.Sub_Trait.2L[[1]]
+Pred.Sub_Trait.2L.testing<-Pred.Sub_Trait.2L[[2]]
+#1.3.The third layer for predicted Sub_Trait to EW 
+Pred.EW.3L<-testing.LASSO(Pred.Sub_Trait.2L.394,Pred.Sub_Trait.2L.testing,EW.394,'Phe2EW.3L')
 Predictability.Phe2EW.3L<-evaluation(Pred.EW.3L[[2]],test.EW)
 save(Predictability.Phe2EW.3L,file="/home/yyzhou/CUBIC/5wSNP/result/Predictability.testing/Predictability.testing/Predictability.Phe2EW.3L.Rdata")
 
@@ -244,18 +244,18 @@ Pred.G2EW<-testing.LASSO(snp.394,test.snp,EW.394,'G2EW')
 Predictability.G2EW<-evaluation(Pred.G2EW[[2]],test.EW)
 save(Predictability.G2EW,file="/home/yyzhou/CUBIC/5wSNP/result/Predictability.testing/Predictability.testing/Predictability.G2EW.Rdata")
 
-#4.construct model for predicted GPG_19Phe to EW  
-Pred.GPG_19Phe.394<-cbind(Pred.GPGs.1L.394,Pred.19Phe.2L.394)
-Pred.GPG_19Phe.testing<-cbind(Pred.GPGs.1L.testing,Pred.19Phe.2L.testing)
-Pred.GPG_19Phe2EW<-testing.LASSO(Pred.GPG_19Phe.394,Pred.GPG_19Phe.testing,EW.394,'G2GPG_19Phe2EW')
-Predictability.G2GPG_Phe2EW<-evaluation(Pred.GPG_19Phe2EW[[2]],test.EW)
-save(Predictability.G2GPG_Phe2EW,file="/home/yyzhou/CUBIC/5wSNP/result/Predictability.testing/Predictability.testing/Predictability.G2GPG_Phe2EW.Rdata")
+#4.construct model for predicted GPG&Sub_Trait to EW  
+Pred.GPG&Sub_Trait.394<-cbind(Pred.GPGs.1L.394,Pred.Sub_Trait.2L.394)
+Pred.GPG&Sub_Trait.testing<-cbind(Pred.GPGs.1L.testing,Pred.Sub_Trait.2L.testing)
+Pred.GPG&Sub_Trait2EW<-testing.LASSO(Pred.GPG&Sub_Trait.394,Pred.GPG&Sub_Trait.testing,EW.394,'G2GPG&Sub_Trait2EW')
+Predictability.G2GPG&Sub_Trait2EW<-evaluation(Pred.GPG&Sub_Trait2EW[[2]],test.EW)
+save(Predictability.G2GPG&Sub_Trait2EW,file="/home/yyzhou/CUBIC/5wSNP/result/Predictability.testing/Predictability.testing/Predictability.G2GPG&Sub_Trait2EW.Rdata")
 
-#5.construct model for 19Phe to EW
-id.test<-rownames(test.19Phe)[complete.cases(test.19Phe)]  #remove NA in the 19Phe,leaving 998 lines
+#5.construct model for Sub_Trait to EW
+id.test<-rownames(test.Sub_Trait)[complete.cases(test.Sub_Trait)]  #remove NA in the Sub_Trait,leaving 998 lines
 test.snp.998<-test.snp[id.test,]
-test.19Phe.998<-test.19Phe[id.test,]
+test.Sub_Trait.998<-test.Sub_Trait[id.test,]
 test.EW.998<-as.matrix(test.EW[id.test,])
-Pred.19Phe2EW<-testing.LASSO(Phe.19.394,test.19Phe.998,EW.394,'Phe2EW')
-Predictability.Phe2EW<-evaluation(Pred.19Phe2EW[[2]],test.EW.998)
-save(Predictability.Phe2EW,file="/home/yyzhou/CUBIC/5wSNP/result/Predictability.testing/Predictability.testing/Predictability.Phe2EW.Rdata")
+Pred.Sub_Trait2EW<-testing.LASSO(Sub_Trait.394,test.Sub_Trait.998,EW.394,'Phe2EW')
+Predictability.Sub_Trait2EW<-evaluation(Pred.Sub_Trait2EW[[2]],test.EW.998)
+save(Predictability.Sub_Trait2EW,file="/home/yyzhou/CUBIC/5wSNP/result/Predictability.testing/Predictability.testing/Predictability.Sub_Trait2EW.Rdata")
